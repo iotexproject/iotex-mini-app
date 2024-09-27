ffbd73e0050c8cf6b50f388c2d67c0d750e1abe4
Jane <mortaldodo@163.com>
2024-09-27 18:50:06 +0800

    update menu

--- src/components/Layout/Footer.tsx
+++ src/components/Layout/Footer.tsx
@@ -1,13 +1,16 @@
 import { Icon } from '@iconify/react';
+import { useRouter } from 'next/router';
 
 const Footer = ({ className }: { className?: string }) => {
+  const router = useRouter()
+
   return (
-    <div className="fixed bottom-0 left-0 right-0 bg-black shadow border-t-1 border-[#000] text-white flex justify-around px-4 py-2">
-      <a href="/" className="text-center flex flex-col items-center">
+    <div className="fixed bottom-0 left-0 right-0 bg-black shadow border-t-1 border-[#000] flex justify-around px-4 py-2">
+      <a href="/" className={`text-center flex flex-col items-center ${router.pathname === '/' ? 'text-secondary' : 'text-white'}`}>
         <Icon icon="mdi:home-outline" className='w-5 h-5' />
         <div>Home</div>
       </a>
-      <a href="/share" className="text-center flex flex-col items-center">
+      <a href="/tasks" className={`text-center flex flex-col items-center ${router.pathname === '/tasks' ? 'text-secondary' : 'text-white'}`}>
         <Icon icon="material-symbols:share-outline" className='w-5 h-5' />
         <div>Share</div>
       </a>
