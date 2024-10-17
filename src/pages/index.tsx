40c32e68ea0c859be5f7076cf96b7a2bb6acd6fb
DLHTX <949729789@qq.com>
2024-10-17 14:54:20 +0800

    update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -53,8 +53,7 @@ const Home = observer(() => {
               //approce example
               const tx = await wallet.sendTransaction({
                 data:'0x095ea7b3000000000000000000000000610cbda6f0037b4141a5b949f56479106becb1e9000000000000000000000000000000000000000000000000000000000000000a',
-                to: '0xa00744882684c3e4747faefd68d283ea44099d03',
-                value: 1e18.toString()
+                to: '0xa00744882684c3e4747faefd68d283ea44099d03'
               })
               alert(tx)
             }}>SendTransation</Button>
